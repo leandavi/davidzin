8a595b18e34d120c75a9c33b273232fa137de1a3
lean <chaterdbirulebay@gmail.com>
2021-04-22 08:39:34 -0300

    First commit

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 10 > Acessar recursos ViP
+-Rp. 20 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/5571996452136 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO LEAN⁷$X DAVID 🐊🚩 :*
+_https: https://chat.whatsapp.com/DWQmDEkLngNF2ATBmSbQBX'
+}
+exports.daftarvip = daftarvip
